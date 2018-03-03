6f3083486efb6ad9a951beac42c22853a43c2672
Eduardo Morales <eduardomoberti@hotmail.com>
2018-03-03 23:08:35 +0100

    Get json from different origin

--- www/js/index.js
+++ www/js/index.js
@@ -16,71 +16,52 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 var app = {
 // Application Constructor
   initialize: function () {
-    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-  },
-  // deviceready Event Handler
-  //
-  // Bind any cordova events here. Common events are:
-  // 'pause', 'resume', etc.
-  onDeviceReady: function () {
-    this.actions();
+    //document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    this.onDeviceReady();
   },
 
-  transformText: function () {
-    $("#white-eyes").click(function () {
-      var txt = $("#origin").val();
-      txt = txt.replace(/[aeiouáéíóú]/igm, "i");
-      $("#destiny").val(txt);
+  json: "",
+
+  getJson: function (file) {
+    $.getJSON(file, function (data) {
+      $("#destiny").val(JSON.stringify(data));
     });
+   
   },
 
-  copy: function () {
-    $("#copy").click(function () {
-      $("#destiny").select();
-      document.execCommand("Copy");
-      navigator.notification.beep(2);
-      navigator.notification.alert(
+  onDeviceReady: function () {
+    $(document).ready(function () {
+      $("#white-eyes").click(function () {
+        var txt = $("#origin").val();
+        txt = txt.replace(/[aeiouáéíóú]/igm, "i");
+        $("#destiny").val(txt);
+       });
+
+      $("#copy").click(function () {
+        $("#destiny").select();
+        document.execCommand("Copy");
+        navigator.notification.beep(2);
+        navigator.notification.alert(
           'Has copiado el texto!',
           null,
           'Copiado',
           ['Ok']
           );
-    });
-  },
+      });
 
-  actions: function () {
-    $(document).ready(function () {
-      this.transformText();
-      this.copy();
+      $("#json").click(function () {
+        app.getJson("json/words.json")
+      });
+      $("#json2").click(function () {
+        app.getJson("https://raw.githubusercontent.com/Eduardomoberti/iparser/master/www/json/words.json")
+      });
+
     });
   }
+
 };
 app.initialize();
-
-//      var json = this.getJson("https://raw.githubusercontent.com/Eduardomoberti/iparser/master/www/json/words.json");
-//      if(!json) {
-//        json = this.getJson("json/words.json");
-//      }
-//      navigator.notification.alert(
-//        json,
-//        null,
-//        'JSON',
-//        ['Ok']
-//      );
-
-//  getJson: function (file) {
-//    $.getJSON(file)
-//      .done(function(data) {
-//        return data;
-//      })
-//      .fail(function() {
-//        console.log( "error, file not found" );
-//        return false;
-//      })
-//      .always(function() {
-//        console.log( "complete" );
-//      });
-//  },
